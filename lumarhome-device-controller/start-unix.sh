2401281ba99e3547884f5e79e4f9aa0ba5cc1925
mbans <martinbans@gmail.com>
2017-05-18 01:05:04 +0100

    Add pythin ve command

--- lumarhome-device-controller/start-unix.sh
+++ lumarhome-device-controller/start-unix.sh
@@ -1,2 +1,3 @@
+source /home/pi/deployments/device-discovery/device-discovery-ve/bin/activat
 cd /home/pi/apps/lumarhome-device-controller/lumarhome-device-controller/
 nohup python lumarhome_rest.py &
