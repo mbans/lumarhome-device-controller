a5316b521da6cad22dec9bc450e952be14d0c464
mbans <martinbans@gmail.com>
2017-05-26 07:32:26 +0800

    Add stop and start

--- lumarhome-device-controller/start-unix.sh
+++ lumarhome-device-controller/start-unix.sh
@@ -1,3 +1,1 @@
-source /home/pi/deployments/device-discovery/device-discovery-ve/bin/activat
-cd /home/pi/apps/lumarhome-device-controller/lumarhome-device-controller/
-nohup python lumarhome_rest.py &
+pkill python
---
+++ lumarhome-device-controller/stop.sh
@@ -0,0 +1,3 @@
+source /home/pi/deployments/device-discovery/device-discovery-ve/bin/activat
+cd /home/pi/apps/lumarhome-device-controller/lumarhome-device-controller/
+nohup python lumarhome_rest.py &
