8f9be55b10d941a4f3f14558272cc89c79895eed
mbans <martinbans@gmail.com>
2017-05-26 07:34:37 +0800

    Amend stop start scripts

--- lumarhome-device-controller/start-unix.sh
+++ lumarhome-device-controller/start-unix.sh
@@ -1,1 +1,3 @@
-pkill python
+source /home/pi/deployments/device-discovery/device-discovery-ve/bin/activate
+cd /home/pi/apps/lumarhome-device-controller/lumarhome-device-controller/
+nohup python lumarhome_rest.py &
